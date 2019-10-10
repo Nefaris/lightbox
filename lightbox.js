c4cb344e10ad1c2cf365c1faccbe5ac34282f20e
Nefaris <kamcio190@gmail.com>
2019-10-10 17:28:42 +0200

    able to group images

--- lightbox.js
+++ lightbox.js
@@ -1,6 +1,3 @@
-let imageSources = [];
-let actualImage; 
-
 const lightboxWrapper = document.getElementById("lightbox-wrapper");
 const lightboxBodyCover = document.getElementById("lightbox-body-cover");
 const imageCounter = document.getElementById('image-counter');
@@ -8,13 +5,6 @@ const lightboxImage = document.getElementById('lightbox-image');
 const lightboxPrevButton = document.getElementById('lightbox-prev-button');
 const lightboxNextButton = document.getElementById('lightbox-next-button');
 const lightboxCloseButton = document.getElementById('lightbox-close-button');
-
-let lightboxImaes = Array.from(document.getElementsByClassName("lightbox"));
-
-for (let image of lightboxImaes) {
-    imageSources.push(image.src);
-    image.addEventListener('click', openLightbox);
-}
 
 lightboxBodyCover.addEventListener('click', hideLightboxWrapper);
 lightboxPrevButton.addEventListener('click', setPrevImage);
@@ -24,46 +14,43 @@ lightboxCloseButton.addEventListener('click', hideLightboxWrapper);
 window.addEventListener('keyup', keyPressed);
 
 function keyPressed(event) {
-    // left arrow: 37
-    // right arrow: 39
-    
-    if (event.keyCode == 39) {
+    if (event.keyCode === 39) {
         setNextImage();
-    } else if (event.keyCode == 37) {
+    } else if (event.keyCode === 37) {
         setPrevImage();
     }
 }
 
-function openLightbox(e) {
-    actualImage = e.target.src;
-    setLightboxImage(actualImage);
-    updateImageCounter();
+function openLightbox(image) {
+    setLightboxImage(image.src);
     showLightboxWrapper();
-}
-
-function setPrevImage() {
-    if (imageSources.indexOf(actualImage) > 0) {
-        actualImage = imageSources[imageSources.indexOf(actualImage) - 1];
-        setLightboxImage(actualImage);
-        updateImageCounter();
-    }
-}
-
-function setNextImage() {
-    if (imageSources.indexOf(actualImage) < imageSources.length - 1) {
-        actualImage = imageSources[imageSources.indexOf(actualImage) + 1]
-        setLightboxImage(actualImage);
-        updateImageCounter();
-    }
 }
 
 function setLightboxImage(image) {
     lightboxImage.innerHTML = `<img src=${image}>`
 }
 
-function updateImageCounter() {
-    let number = imageSources.indexOf(actualImage) + 1;
-    imageCounter.innerHTML = `${number} / ${imageSources.length}`;
+let actualCat;
+let actualPhotoIndex = 0;
+
+function setPrevImage() {
+    if (actualPhotoIndex > 0) {
+        actualPhotoIndex--;
+        setLightboxImage(lightboxImages.get(actualCat)[actualPhotoIndex].src);
+        setCounter(actualPhotoIndex + 1, lightboxImages.get(actualCat).length);
+    }
+}
+
+function setNextImage() {
+    if (actualPhotoIndex < lightboxImages.get(actualCat).length - 1) {
+        actualPhotoIndex++;
+        setLightboxImage(lightboxImages.get(actualCat)[actualPhotoIndex].src);
+        setCounter(actualPhotoIndex + 1, lightboxImages.get(actualCat).length);
+    }
+}
+
+function setCounter(actual, max) {
+    imageCounter.innerHTML = `${actual} / ${max}`;
 }
 
 function showLightboxWrapper() {
@@ -73,3 +60,27 @@ function showLightboxWrapper() {
 function hideLightboxWrapper() {
     lightboxWrapper.style.display = "none";
 }
+
+const images = document.querySelectorAll('.lightbox');
+
+let lightboxImages = new Map();
+
+for (const image of images) {
+    if (lightboxImages.has(image.dataset.lightbox)) {
+        lightboxImages.get(image.dataset.lightbox).push(image);
+    } else {
+        lightboxImages.set(image.dataset.lightbox, [image]);
+    }
+}
+
+lightboxImages.forEach((v, k) => {
+    for (const image of v) {
+        image.addEventListener('click', () => {
+            actualPhotoIndex = v.indexOf(image);
+            actualCat = k;
+            setCounter(actualPhotoIndex + 1, v.length);
+            openLightbox(image);
+        });
+    }
+});
+
